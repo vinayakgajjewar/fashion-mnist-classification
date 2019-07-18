7b1ff78aada71ec766917ac5e17246d19e3911cb
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-07-17 17:42:57 -0700

    Add two hidden layers in an attempt to make the model more accurate

--- nn.py
+++ nn.py
@@ -62,6 +62,8 @@ for i in range(25):
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
